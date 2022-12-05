5fea3fe6b8edf6a659890cf96e6862f04385082c
VictoriaKonovalova <112749149+VictoriaKonovalova@users.noreply.github.com>
2022-12-06 02:59:07 +0300

    hard

--- PyCharm/hard.py
+++ PyCharm/hard.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3
+#!\usr\bin\env python3
 # -*- coding: utf-8 -*-
 
-import math
 import sys
+import math
 
 # Постоянная Эйлера.
 EULER = 0.5772156649015328606
@@ -10,4 +10,15 @@ EULER = 0.5772156649015328606
 EPS = 1e-10
 
 if __name__ == '__main__':
-    print(EPS)
+    x = float(input("Value of x? "))
+    if x == 0:
+        print("Illegal value of x", file=sys.stderr)
+        exit(1)
+    a = x
+    S, n = a, 1
+    while math.fabs(a) > EPS:
+        a *= ((x ** 2) * (2 * n + 1)) / (((2 * n + 3) ** 2) * (2 * n + 2))
+        S += a
+        n += 1
+
+print(f"Ei({x}) = {EULER + math.log(math.fabs(x)) + S}")
